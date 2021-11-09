ab60c13cc326aa3702d7fc4a3670d1e891044f30
Jeannie <jntsao@gmail.com>
2021-11-08 18:24:29 -0800

    created rendercustomview function to check if the currentMessage contains location data

--- components/Chat.js
+++ components/Chat.js
@@ -344,7 +344,22 @@ export default class Chat extends React.Component {
   renderCustomView(props) {
     const { currentMessage } = props;
     if (currentMessage.location) {
-
+      return (
+        <MapView
+          style={{
+            width: 150,
+            height: 100,
+            borderRadius: 13,
+            margin: 3
+          }}
+          region={{
+            latitude: currentMessage.location.latitude,
+            longitude: currentMessage.location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      );
     }
   }
 
